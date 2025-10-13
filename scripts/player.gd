1a42774de3b282137c1f8c3e466246c0f2137fa7
Jiwwi <lazyjiwwi@gmail.com>
2025-10-13 16:41:13 +0200

    Add player movement and input, update scenes
    Implemented basic player movement in player.gd using new input actions defined in project.godot. Updated player.tscn to use a CapsuleShape2D for collision, added a Camera2D, and adjusted sprite scaling. Added a new test object scene and instanced both the player and test object in the house scene.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,1 +1,21 @@
 extends CharacterBody2D
+
+var speed = 400
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("move_right"):
+		velocity.x += speed*delta
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= speed*delta
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= speed*delta
+	if Input.is_action_pressed("move_down"):
+		velocity.y += speed*delta
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		
+	move_and_slide()
