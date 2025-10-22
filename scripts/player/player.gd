c34d78ccc497b2a65a533315db3e269f7f285d3d
Jiwwi <lazyjiwwi@gmail.com>
2025-10-22 18:30:01 +0200

    Update player.gd

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -2,21 +2,21 @@ extends CharacterBody2D
 
 var speed = 200
 
-func _physics_process(delta: float) -> void:
-	var input_vector = Vector2.ZERO
+func _process(_delta: float) -> void:
+	velocity = Vector2.ZERO
 	
 	if Input.is_action_pressed("move_right"):
-		input_vector.x += 1
+		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
-		input_vector.x -= 1
+		velocity.x -= 1
 	if Input.is_action_pressed("move_up"):
-		input_vector.y -= 1
+		velocity.y -= 1
 	if Input.is_action_pressed("move_down"):
-		input_vector.y += 1
+		velocity.y += 1
 
-	if input_vector.length() > 0:
-		input_vector = input_vector.normalized()
+	if velocity.length() > 0:
+		velocity = velocity.normalized()
 
-	velocity = input_vector * speed
+	velocity = velocity * speed
 
 	move_and_slide()
