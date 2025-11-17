e0fc2272658a34fa904207a4cfb2922640325ab2
Jiwwi <lazyjiwwi@gmail.com>
2025-11-17 16:41:59 +0100

    Move interaction check to _process and add zone flag
    Introduces a 'zone' variable to track when the player is inside the board area. The interaction input check is now performed in _process instead of _on_body_entered, allowing interaction only while the player remains in the zone.

--- scripts/environment/interactables/board.gd
+++ scripts/environment/interactables/board.gd
@@ -1,7 +1,15 @@
 extends Area2D
 
+var zone = false
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		print("entered")
-		if Input.is_action_just_pressed("interact"):
-			print("interacted")
+		zone = true
+	else:
+		zone = false
+
+func _process(_delta: float) -> void:
+	if zone == true and Input.is_action_just_pressed("interact"):
+		print("interacted")
+		
