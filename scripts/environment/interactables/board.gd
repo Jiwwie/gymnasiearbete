a0035fe0e53711f5aef317a1e69188e866a35c8a
Jiwwi <lazyjiwwi@gmail.com>
2025-11-19 16:08:37 +0100

    Add board UI to house scene and update board interaction
    Introduces a new BoardUi CanvasLayer to the house scene and updates the board.gd script to show/hide the board UI when interacting with the board. The board_ui.tscn scene is refactored to use a CenterContainer and TextureRect for improved layout and appearance.

--- scripts/environment/interactables/board.gd
+++ scripts/environment/interactables/board.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 var zone = false
 @onready var interact_text: CanvasLayer = %InteractText
+@onready var board_ui: CanvasLayer = %BoardUi
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
@@ -14,8 +15,10 @@ func _on_body_exited(body: Node2D) -> void:
 		print("left")
 		zone = false
 		interact_text.hide()
+		board_ui.hide()
 	
 func _process(_delta: float) -> void:
 	if zone == true and Input.is_action_just_pressed("interact"):
-		print("interacted")
+		board_ui.show()
+		
 		
