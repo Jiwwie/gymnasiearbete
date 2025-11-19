def6473cf50b40c3f89f215cb5952f95d8768519
Jiwwi <lazyjiwwi@gmail.com>
2025-11-19 15:33:01 +0100

    Add board exit signal and handler
    Connected the 'body_exited' signal for the Board node in house.tscn and implemented the _on_body_exited method in board.gd to handle player exit events, updating the zone state and logging when the player leaves the board area.

--- scripts/environment/interactables/board.gd
+++ scripts/environment/interactables/board.gd
@@ -6,9 +6,12 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		print("entered")
 		zone = true
-	else:
-		zone = false
 
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		print("left")
+		zone = false
+	
 func _process(_delta: float) -> void:
 	if zone == true and Input.is_action_just_pressed("interact"):
 		print("interacted")
