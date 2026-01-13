f080bf69d1ab85fa3e48742915859478285c66d5
Jiwwi <lazyjiwwi@gmail.com>
2026-01-13 19:18:50 +0100

    Add bridge exit and update player idle animation
    Introduced a new bridge exit in the environment with corresponding scene and script to handle player transitions to the house. Updated player idle animation to use 'idle_up' when dialogue is active. Adjusted player and spawn positions in house and outside scenes to accommodate the new bridge exit.

---
+++ scripts/environment/exits/bridge.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+var target_scene = "res://scenes/rooms/house.tscn"
+
+func _ready() -> void:
+	pass
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		call_deferred("_change_scene")
+		SpawnManager.spawn = true
+
+func _change_scene():
+	get_tree().change_scene_to_file(target_scene)
