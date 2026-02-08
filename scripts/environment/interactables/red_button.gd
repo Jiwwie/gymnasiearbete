bb376781f45ba1dad5f36032d6c808396133bb5b
Jiwwi <lazyjiwwi@gmail.com>
2026-02-08 14:01:40 +0100

    Support dynamic ending scenes and spawn
    Move the original BadEnding scene content into a new maik1.tscn and simplify bad_ending.tscn (now a GoodEnding root). Add a spawn Marker2D to red_button.tscn and update to_ending.tscn to target the new maik1 scene. Update scripts/environment/interactables/red_button.gd to export target_scene and spawn, pick the target scene based on Globals.ending, show the interact UI, handle the interact action, and call SceneManager.change_scene(target_scene, spawn). These changes enable switching between good/bad endings and support spawning when changing scenes.

--- scripts/environment/interactables/red_button.gd
+++ scripts/environment/interactables/red_button.gd
@@ -2,8 +2,26 @@ extends Area2D
 
 var zone = false
 @onready var interact_text: CanvasLayer = %InteractText
+@export var target_scene: String
+@export var spawn: bool
+
+func _ready() -> void:
+	spawn = true
+	if Globals.ending == "good":
+		target_scene = "good_ending"
+	else:
+		target_scene = "bad_ending"
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		zone = true
-		interact_text.show()
+		interact_text.show()	
+
+func _process(_delta: float) -> void:
+	if zone == true and Input.is_action_just_pressed("interact"):
+		call_deferred("_change_scene")
+
+func _change_scene():
+	SceneManager.change_scene(target_scene, spawn)
+	
+	
